74f2a91ee4496fb5d49657accdb0a7ce112f3db5
KrzzLpzz <crisam1801@gmail.com>
2024-02-05 12:37:03 -0600

    uodates

--- ToDoList/ToDoList.Designer.vb
+++ ToDoList/ToDoList.Designer.vb
@@ -360,7 +360,7 @@ Partial Class ToDoList
         'Timer1
         '
         Me.Timer1.Enabled = True
-        Me.Timer1.Interval = 10000
+        Me.Timer1.Interval = 20000
         '
         'ToDoList
         '
